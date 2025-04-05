5433f44cd383162b0b1a3a7471bb58748dcff192
clickcatayst <hello.clickcatalyst@gmail.com>
2025-04-05 20:54:30 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './python-service/templates/**/*.html', // Scans original templates
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
